a358fd0c7125e88ee55cdfbbee531bd04d67a1d9
jgmagnani <jogamabnu@gmail.com>
2022-10-19 17:33:55 -0300

    Implementado rotina de verificar quantidade de cartas

--- scriptsJS/script.js
+++ scriptsJS/script.js
@@ -1,1 +1,9 @@
 
+let qtdCards = Number(prompt("Quantas cartas desejas jogar? Valores entre 4 e 14 (somente par)"));
+
+while ( qtdCards > 14 || qtdCards < 4 || qtdCards % 2 !== 0){
+    qtdCards = prompt("Insira um valor entre 4 e 14 (apenas par)")
+    if ( qtdCards % 2 !== 0){
+        alert("O número é impar");
+    }    
+}
