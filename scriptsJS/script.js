5ea29d917e684c303ff05c803353b031c3ceb69d
jgmagnani <jogamabnu@gmail.com>
2022-10-23 23:54:28 -0300

    base projeto

--- scriptsJS/script.js
+++ scriptsJS/script.js
@@ -1,15 +1,15 @@
-
+let jogadas = 0;
 let qtdCards = Number(prompt("Quantas cartas desejas jogar? Valores entre 4 e 14 (somente par)"));
 const gifs = [
-    'bobrossparrot.gif',
-    'explodyparrot.gif',
-    'fiestaparrot.gif',
-    'metalparrot.gif',
-    'revertitparrot.gif',
-    'tripletsparrot.gif',
-    'unicornparrot.gif']
+    'bobrossparrot.gif','bobrossparrot.gif',
+    'explodyparrot.gif', 'explodyparrot.gif',
+    'fiestaparrot.gif', 'fiestaparrot.gif',
+    'metalparrot.gif', 'metalparrot.gif',
+    'revertitparrot.gif', 'revertitparrot.gif',
+    'tripletsparrot.gif', 'tripletsparrot.gif',
+    'unicornparrot.gif', 'unicornparrot.gif']
 
-console.log(gifs)
+
 //Verifica quantidade de cartas dentro dos requisitos (Entre 4 e 14 apenas par)
 while ( qtdCards > 14 || qtdCards < 4 || qtdCards % 2 !== 0){    
     if ( qtdCards % 2 !== 0 && Number(qtdCards)){
@@ -18,23 +18,65 @@ while ( qtdCards > 14 || qtdCards < 4 || qtdCards % 2 !== 0){
     qtdCards = prompt("Insira um valor entre 4 e 14 (apenas par)");
 }
 
-console.log(qtdCards)
 
+// coloca as cartas em um array
 
-// coloca as cartas na tela
-const elemento = document.querySelector('ul');
 let addLi = []
 
 for (let index = 0; index < qtdCards; index++) {     
-    console.log(elemento)
-    elemento.innerHTML += `<li onclick="virarCarta(this)"><img src="imagens/back.png" alt="CARD"></li>`;    
+    
+    addLi.push( `
+    <div onclick="virarCarta(this)" class="cards">
+        <div class="frente lista">
+            <img src="imagens/back.png" alt="CARD">
+        </div>
+        <div class="tras lista">
+            <img src="imagens/${gifs[index]}" alt="CARD">
+        </div>
+    </div>
+    `);   
+    
+}
+//faz o sort das cartas na tela...
+addLi.sort(comparar)
+//coloca as cartas na tela
+const criaCarta = document.querySelector('.cartas');
 
+for (let index = 0; index < addLi.length; index++) {
+    criaCarta.innerHTML += addLi[index]          
 }
 
-
-function virarCarta(elemento){    
-    elemento.classList.toggle('sideCard');
-    const arrunma = elemento;
+//Função criada no click da carta para virar ela
+let cartas = []
+function virarCarta(elemento){   
     
-    arrunma.innerHTML = `<li><img src="imagens/metalparrot.gif" alt="CARD"></li>`
+    elemento.classList.add("sideCard");
+    cartas.push(elemento);
+    
+    jogadas++;
+    
+    if (cartas.length >= 2) {
+        if (cartas[0].innerHTML !== cartas[1].innerHTML) {
+            setTimeout(desvirar, 1000, cartas[0], cartas[1])
+        }        
+        cartas = []     
+    }
+    if(document.querySelectorAll(".sideCard").length == qtdCards){
+        setTimeout(acabou,500)
+    }
+}
+
+function desvirar(carta1, carta2){
+   
+    carta1.classList.remove("sideCard")
+    carta2.classList.remove("sideCard")
+   
+}
+
+function comparar() {
+    return Math.random() - 0.5;
+}
+
+function acabou() {
+    alert('cabo')
 }
